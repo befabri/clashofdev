49398f192597af76bc8157aa0537f07d67cf8781
Benjamin Fabri <bfabri@pm.me>
2024-09-23 16:18:08 +0200

    Add opengraph; update Head

--- astro.config.ts
+++ astro.config.ts
@@ -3,8 +3,10 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import preact from "@astrojs/preact";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), icon(), preact()],
+    site: "https://clashofdev.bfabri.me/",
+    integrations: [tailwind(), icon(), preact(), sitemap()],
 });
