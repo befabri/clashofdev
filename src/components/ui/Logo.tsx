e77416f779742e115d4b6c00fa8fb16bde426747
Benjamin Fabri <bfabri@pm.me>
2024-10-12 18:02:48 +0200

    Update Logo background

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -1,10 +1,7 @@
 export default function Logo() {
     return (
-        <a
-            href="/"
-            aria-label="Accueil"
-            class="bg-cod_black dark:bg-black rounded-full px-4 py-2 rotate-[8deg]">
-            <span class="uppercase text-xs text-purple_light dark:text-purple_light font-bold">LA PISCINE</span>
+        <a href="/" aria-label="Accueil" class="bg-cod_black dark:bg-beige rounded-full px-4 py-2 rotate-[8deg]">
+            <span class="uppercase text-xs text-purple_light dark:text-cod_black font-bold">LA PISCINE</span>
         </a>
     );
 }
