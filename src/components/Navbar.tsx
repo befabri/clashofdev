ef5e864f53e0dca9bfa2cd5b455119b6e565e46b
Benjamin Fabri <bfabri@pm.me>
2024-10-13 19:10:14 +0200

    Fix navbar hiding

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,38 +1,44 @@
 import ScrollPercentage from "./ui/ScrollPercentage";
 import ToggleSwitch from "./ui/ToggleSwitch";
 import Logo from "./ui/Logo";
-import { useEffect, useState } from "preact/hooks";
+import { useEffect, useRef, useState } from "preact/hooks";
 
 interface Props {
     scrollPercent: number;
 }
-
 export default function Navbar({ scrollPercent }: Props) {
     const [isAtBottom, setIsAtBottom] = useState(false);
-    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+    const navbarRef = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
-        const handleResize = () => {
-            setWindowWidth(window.innerWidth);
+        const ctaElement = document.getElementById("cta");
+
+        const checkIfAtBottom = () => {
+            const navbarElement = navbarRef.current;
+            if (!navbarElement || !ctaElement) return;
+
+            const navbarRect = navbarElement.getBoundingClientRect();
+            const ctaRect = ctaElement.getBoundingClientRect();
+
+            if (navbarRect.top < 40 && ctaRect.y <= navbarRect.y + 46) {
+                setIsAtBottom(true);
+            } else if (ctaRect.y <= navbarRect.y + 50) {
+                setIsAtBottom(true);
+            } else {
+                setIsAtBottom(false);
+            }
         };
 
-        window.addEventListener("resize", handleResize);
-
-        return () => {
-            window.removeEventListener("resize", handleResize);
-        };
-    }, []);
-
-    useEffect(() => {
-        if (windowWidth <= 880) {
-            setIsAtBottom(Math.round(scrollPercent) >= 99);
+        if (Math.round(scrollPercent) >= 99) {
+            setIsAtBottom(true);
         } else {
-            setIsAtBottom(Math.round(scrollPercent) >= 97);
+            checkIfAtBottom();
         }
     }, [scrollPercent]);
 
     return (
         <div
+            ref={navbarRef}
             class={`mx-auto max-w-screen-2xl w-full flex flex-row items-center h-[39px] sm:h-[39.4px] px-3 sm:px-8 transition-all duration-150 ease-in ${
                 isAtBottom ? "opacity-0 transform" : "opacity-100 transform"
             }`}>
